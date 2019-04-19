7351769225e288c5fbfdad94f60693568261a551
Joseph Hejderup <j.i.hejderup@tudelft.nl>
2019-04-19 20:51:35 +0200

    Update GenerateMavenCG2.java

--- src/main/java/com/github/jhejderup/GenerateMavenCG2.java
+++ src/main/java/com/github/jhejderup/GenerateMavenCG2.java
@@ -1,29 +1,20 @@
 package com.github.jhejderup;
 
-import com.github.jhejderup.data.callgraph.ResolvedCall;
+
 import com.github.jhejderup.data.callgraph.WalaCallGraph;
-import com.github.jhejderup.data.type.MavenCoordinate;
 import com.github.jhejderup.data.type.MavenResolvedCoordinate;
 import com.github.jhejderup.generator.WalaCallgraphConstructor;
-import guru.nidi.graphviz.engine.Format;
-import guru.nidi.graphviz.engine.Graphviz;
-import guru.nidi.graphviz.engine.GraphvizCmdLineEngine;
-import guru.nidi.graphviz.model.Graph;
-import guru.nidi.graphviz.model.LinkSource;
 import org.jboss.shrinkwrap.resolver.api.maven.Maven;
 import org.jboss.shrinkwrap.resolver.api.maven.MavenResolvedArtifact;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.File;
-import java.util.ArrayList;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import static guru.nidi.graphviz.model.Factory.graph;
-import static guru.nidi.graphviz.model.Factory.node;
-import static guru.nidi.graphviz.model.Link.to;
+
 
 public class GenerateMavenCG2 {
 
@@ -42,28 +33,15 @@ public class GenerateMavenCG2 {
 
 
     public static void main(String[] args) {
-
-
         logger.info("Constructing generator of package {}", args[1]);
-
-        final GraphvizCmdLineEngine engine = new GraphvizCmdLineEngine();
-
-        engine.setDotOutputFile(args[0],args[1]);
-
         try {
             List<MavenResolvedCoordinate> path = buildClasspath(args[1]);
             logger.info("Building generator using generator....");
-           WalaCallGraph cg =  WalaCallgraphConstructor.build(path);
-           WalaUFIAdapter wrapped_cg = WalaUFIAdapter.wrap(cg);
+            WalaCallGraph cg =  WalaCallgraphConstructor.build(path);
+            WalaUFIAdapter wrapped_cg = WalaUFIAdapter.wrap(cg);
             logger.info("Call graph construction done!");
-            logger.info("Convert it to graphviz");
-            Graph g = graph().directed().with(WalaCallgraphConstructor.resolveCalls(wrapped_cg.callGraph.rawcg).stream().map(
-                    call -> node(wrapped_cg.convertToUFI(call.source).toString())
-                            .link(to(node(wrapped_cg.convertToUFI(call.target).toString())))
-            ).toArray(LinkSource[]::new));
-            logger.info("Convertion done, save call graph to {}", args[1]);
-            Graphviz.useEngine(engine);
-            Graphviz.fromGraph(g).render(Format.PLAIN).toString();
+            logger.info("Save it to file");
+            wrapped_cg.toFile(args[0],args[1] + ".dot");
         } catch (Exception e) {
             logger.error("An exception occurred for {}",args[1], e);
         }
