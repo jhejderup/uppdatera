293dab9dc7bf382704836fb2b1f1580a119c4340
Joseph Hejderup <jhejderup@users.noreply.github.com>
2019-07-02 14:30:53 +0200

    Generate function calls

--- src/main/java/FetchGithubProjects.java
+++ src/main/java/FetchGithubProjects.java
@@ -1,12 +1,53 @@
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
 import org.kohsuke.github.GHContent;
+import org.kohsuke.github.GHRepository;
 import org.kohsuke.github.GitHub;
 
+import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.Objects;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
 public class FetchGithubProjects {
+
+   private static final String CODECOV  = "https://codecov.io/gh/";
+   private static final String COVERALLS = "https://coveralls.io/";
+    private static final String CODECLIMATE = "https://codeclimate.com/github/";
+
+
+
+    public static boolean hasTravisCI(GHRepository repo) {
+        try {
+            repo.getFileContent(".travis.yml");
+            return true;
+        } catch (IOException e) {
+            return false;
+        }
+    }
+
+    public static boolean hasCodeCoverageService(GHRepository repo) {
+        try {
+            var content = repo.getFileContent("README.md");
+            var reader = new BufferedReader(new InputStreamReader(content.read()));
+            while(reader.ready()) {
+                String line = reader.readLine();
+                if(line.contains(CODECOV) || line.contains(COVERALLS) || line.contains(CODECLIMATE))
+                    return true;
+            }
+            return false;
+        } catch (IOException e) {
+            return false;
+        }
+    }
 
 
 
@@ -15,29 +56,85 @@ public class FetchGithubProjects {
 
         System.out.println(github.getRateLimit());
 
-        var result = github.searchContent()
-                .q("gradle/dependency-locks")
-                .in("path").list();
+
+//        var path = Paths.get("/Users/jhejderup/crap2.txt");
+
+
+//        Files.readAllLines(path)
+//                .stream()
+//                .map(l -> l.split(","))
+//                .map(ls ->String.format("https://github.com/%s/compare/%s..%s", ls[0],ls[1],ls[3]))
+//                .forEach(System.out::println);
+
+
+//        Files.readAllLines(path)
+//                .stream()
+//                .map(l -> l.split(","))
+//                .map(ls -> {
+//                    try {
+//                        var nonbuildCount = Arrays.stream(github
+//                                .getRepository(ls[0])
+//                                .getCompare(ls[1],ls[3])
+//                                .getFiles())
+//                                .filter(k -> !k.getFileName().contains("pom.xml") && !k.getFileName().contains("build.gradle"))
+//                                .count();
+//
+//                        if (nonbuildCount > 0){
+//                            return null;
+//                        } else {
+//                            return ls;
+//                        }
+//                    } catch (IOException e) {
+//                        return null;
+//                    }
+//                })
+//                .filter(Objects::nonNull)
+//                .map(ls -> String.join(",", ls))
+//                .forEach(System.out::println);
+
+
+
+//        var files = github.getRepository("gwtbootstrap3/gwtbootstrap3")
+//                .getCompare("a04344eda3f16bcd70d822b03c2dfdf4e639848e","a96eb983b06ca5e271b5393b8710c46c6f105dc0")
+//                .getFiles();
+
+
+
+
+        var repos = github
+                .searchRepositories()
+                .language("java")
+                .stars("100..500")
+                .list();
 
 
         var repositories = StreamSupport
-                .stream(result.spliterator(), false)
-                .map(r -> r.getOwner())
-                .distinct()
-                .collect(Collectors.toList());
+                .stream(repos.spliterator(), false);
 
-        repositories.stream()
-                .map(repo -> {
-                    try {
-                        return github.getRepository(repo.getFullName());
-                    } catch (IOException e) {
-                        return null;
-                    }
-                })
-                .filter(Objects::nonNull)
+
+        repositories
                 .filter(repo -> Objects.equals(repo.getLanguage(), "Java"))
-                .map(repo -> repo.getHtmlUrl())
-                .forEach(System.out::println);
+                .filter(FetchGithubProjects::hasTravisCI)
+                .filter(FetchGithubProjects::hasCodeCoverageService)
+                .forEach(repo -> System.out.println(String.format("%s.git", repo.getHtmlUrl().toString())));
+
+
+//        repositories
+//                .filter(repo -> Objects.equals(repo.getLanguage(), "Java"))
+//                .filter(FetchGithubProjects::hasTravisCI)
+//                .forEach(repo -> {
+//
+//
+//                    try {
+//                        var f = new File(String.format("/Users/jhejderup/Studies/uppdatera/%s", repo.getFullName()));
+//                        Git.cloneRepository().setURI(String.format("%s.git", repo.getHtmlUrl().toString()))
+//                                .setDirectory(f)
+//                                .call();
+//                        System.out.println(String.format("%s.git", repo.getHtmlUrl().toString()));
+//                    } catch (GitAPIException e) {
+//                        e.printStackTrace();
+//                    }
+//                });
 
 
 
