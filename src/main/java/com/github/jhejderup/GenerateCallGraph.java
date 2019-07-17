b47f3f5632d2fefe5c429a1ed92095726d4fa435
Joseph Hejderup <j.i.hejderup@tudelft.nl>
2019-07-17 16:21:42 +0200

    Just resolve and print dependencies

--- src/main/java/com/github/jhejderup/GenerateCallGraph.java
+++ src/main/java/com/github/jhejderup/GenerateCallGraph.java
@@ -21,28 +21,12 @@ public class GenerateCallGraph {
 
     private static Logger logger = LoggerFactory.getLogger(GenerateCallGraph.class);
 
-    private static boolean isMavenProject(Path repository) {
-        var pomFile = Paths.get(repository.toString(), "pom.xml");
-        return Files.exists(pomFile);
-    }
-
-
-    private static boolean isGradleProject(Path repository) {
-        var gradleFile = Paths.get(repository.toString(), "build.gradle");
-        return Files.exists(gradleFile);
-    }
-
-
     public static void main(String[] args) {
 
         //0. Seperate input
-        var readPaths = args[0].split(" ");
-
-        var pomXML = readPaths[0];
-        var appJAR = readPaths[1];
+        var pomXML = args[0];
         logger.info("pom.xml located at {}", pomXML);
-        logger.info("application jar located at {}", appJAR);
-
+      
         //2. Resolve dependencies of pom.xml files
         try {
             var depzFiles = Maven.resolver()
@@ -50,33 +34,9 @@ public class GenerateCallGraph {
                     .importCompileAndRuntimeDependencies()
                     .resolve()
                     .withTransitivity().asFile();
-
-            var app = new MavenResolvedCoordinate("", "", "", Path.of(appJAR));
-
-            var depz = Arrays.stream(depzFiles).map(k -> k.toPath())
-                    .map(d -> new MavenResolvedCoordinate("", "", "", d))
-                    .collect(Collectors.toList());
-
-
-            var classpath = new ModuleClasspath(app, Optional.of(depz));
-
-
-            var callgraph = WalaCallgraphConstructor.build(classpath);
-
-
-            var fileWriter = new PrintWriter(Path.of(pomXML).getParent().toString() + "/uppdatera-calls.txt");
-
-
-            callgraph.rawGraph.forEach(n -> {
-                if (WalaCallgraphConstructor.isUppdateraMethod(n.getMethod())) {
-                        fileWriter.println(n.getMethod().getSignature());
-                        logger.info(n.getMethod().getSignature());
-                }
-
-            });
-
-            fileWriter.close();
-
+            
+            Arrays.stream(depz).forEach(System.out::println);
+            
 
         } catch (Exception e) {
             logger.error("Failed for {} with exception: {}", pomXML, e);
