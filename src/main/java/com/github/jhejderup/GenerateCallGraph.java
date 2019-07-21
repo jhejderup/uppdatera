a37b9b4ec6d74c5768d306549845655c8aebe251
Joseph Hejderup <j.i.hejderup@tudelft.nl>
2019-07-21 15:42:07 +0200

    Update GenerateCallGraph.java

--- src/main/java/com/github/jhejderup/GenerateCallGraph.java
+++ src/main/java/com/github/jhejderup/GenerateCallGraph.java
@@ -73,7 +73,7 @@ public class GenerateCallGraph {
         } catch (Exception e) {
             System.out.print("");
 //             logger.error("Failed for {} with exception: {}", pomXML, e);
-//             e.printStackTrace();
+             e.printStackTrace();
         }
     }
 
