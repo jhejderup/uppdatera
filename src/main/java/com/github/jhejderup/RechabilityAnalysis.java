a16322d179179dc6056fb3e2318d965e86cfe865
Joseph Hejderup <jhejderup@users.noreply.github.com>
2019-10-11 14:46:05 +0200

    Dump the cha of wala

--- src/main/java/com/github/jhejderup/RechabilityAnalysis.java
+++ src/main/java/com/github/jhejderup/RechabilityAnalysis.java
@@ -3,217 +3,68 @@ package com.github.jhejderup;
 import com.github.jhejderup.data.ModuleClasspath;
 import com.github.jhejderup.data.type.MavenResolvedCoordinate;
 import com.github.jhejderup.generator.WalaCallgraphConstructor;
-import com.ibm.wala.classLoader.IMethod;
 import com.ibm.wala.types.ClassLoaderReference;
 import com.ibm.wala.types.MethodReference;
-import org.jboss.shrinkwrap.resolver.api.maven.embedded.BuiltProject;
-import org.jboss.shrinkwrap.resolver.api.maven.embedded.EmbeddedMaven;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.text.SimpleDateFormat;
-import java.util.*;
-import java.util.function.Function;
+import java.util.Optional;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class RechabilityAnalysis {
 
-    public static int resolveDependencies() {
+  public static String toUppdateraFunctionString(MethodReference ref) {
+    return ref.getDeclaringClass().getName().toString() + "/" + ref
+        .getSelector();
+  }
 
-        BuiltProject b = EmbeddedMaven
-                .forProject("pom.xml")
-                .useDefaultDistribution()
-                .setGoals("dependency:copy-dependencies", "-DincludeScope=runtime")
-                .build();
+  public static ClassLoaderReference getClassLoader(MethodReference m) {
+    return m.getDeclaringClass().getClassLoader();
+  }
 
-        return b.getMavenBuildExitCode();
+  public static void main(String[] args) {
+    ///
+    /// How does it work?
+    ///
+    /// We fill the project-based classes in the Application Loader
+    /// We fill the dependencies in the Extension Loader
 
+    String PROJECT_CLASSES = "target/classes";
+    String PROJECT_DEPS = "target/dependency";
+
+    try {
+      var project = new MavenResolvedCoordinate("", "", "",
+          Path.of(PROJECT_CLASSES));
+      var dependencies = Files.find(Paths.get(PROJECT_DEPS), Integer.MAX_VALUE,
+          (filePath, fileAttr) -> fileAttr.isRegularFile() && filePath
+              .toString().endsWith(".jar")).map(
+          j -> new MavenResolvedCoordinate("", "", "", Paths.get(j.toUri())))
+          .collect(Collectors.toList());
+
+      //Create Classpath
+      var classpath = new ModuleClasspath(project, Optional.of(dependencies));
+
+      //Create call graph
+      var cg = WalaCallgraphConstructor.build(classpath, args[0]);
+
+      //Resolve call graph into a CHA call graph
+      var CHACallgraph = WalaCallgraphConstructor.makeCHA(cg);
+
+      var reverseEdges = CHACallgraph.stream().filter(
+          call -> !getClassLoader(call.target)
+              .equals(ClassLoaderReference.Primordial)).map(
+          rs -> toUppdateraFunctionString(rs.target) + " "
+              + toUppdateraFunctionString(rs.source));
+
+      Files.write(Paths.get(args[0], "cg.txt"),
+          (Iterable<String>) reverseEdges::iterator);
+
+    } catch (Exception e) {
+      e.printStackTrace();
     }
 
-    public static Set<UppdateraMethod> readRecordedFunctions() throws IOException {
-        return Files.lines(Paths.get("functions.txt"))
-                .map(l -> new UppdateraMethod(l, ClassLoaderReference.Extension))
-                .collect(Collectors.toSet());
-    }
+  }
 
-    public static String toUppdateraFunctionString(MethodReference ref) {
-        return ref.getDeclaringClass().getName().toString() + "/" + ref.getSelector();
-    }
-
-    public static Boolean isApplicationOrExtension(IMethod m) {
-        return getClassLoader(m).equals(ClassLoaderReference.Application) ||
-                getClassLoader(m).equals(ClassLoaderReference.Extension);
-    }
-
-    public static ClassLoaderReference getClassLoader(IMethod m) {
-        return m.getReference().getDeclaringClass().getClassLoader();
-    }
-
-    public static ClassLoaderReference getClassLoader(MethodReference m) {
-        return m.getDeclaringClass().getClassLoader();
-    }
-
-    // Generic function to merge two sets in Java
-    public static <T> Set<T> mergeSets(Set<T> a, Set<T> b) {
-        return Stream.of(a, b)
-                .flatMap(x -> x.stream())
-                .collect(Collectors.toSet());
-    }
-
-
-    public static boolean isRechable(UppdateraMethod m, Map<UppdateraMethod, Set<UppdateraMethod>> cg) {
-        var visited = new HashSet<UppdateraMethod>();
-        var queue = new Stack<UppdateraMethod>();
-
-        //Add first value
-        queue.add(m);
-        visited.add(m);
-        //Keep Searching
-        while (queue.size() > 0) {
-            var elem = queue.pop();
-            if (elem.classLoader.equals(ClassLoaderReference.Application))
-                return true;
-            if (cg.containsKey(elem)) {
-                cg.get(elem)
-                        .parallelStream()
-                        .filter(c -> !visited.contains(c))
-                        .forEach(c -> {
-                            visited.add(c);
-                            queue.add(c);
-                        });
-            }
-        }
-        return false;
-    }
-
-
-    public static void main(String[] args) {
-        ///
-        /// How does it work?
-        ///
-        /// We fill the project-based classes in the Application Loader
-        /// We fill the dependencies in the Extension Loader
-
-
-        var depCode = resolveDependencies();
-        assert depCode == 0;
-
-        String PROJECT_CLASSES = "target/classes";
-        String PROJECT_DEPS = "target/dependency";
-
-        try {
-            var functions = readRecordedFunctions();
-            var project = new MavenResolvedCoordinate("", "", "", Path.of(PROJECT_CLASSES));
-            var dependencies = Files.find(Paths.get(PROJECT_DEPS),
-                    Integer.MAX_VALUE,
-                    (filePath, fileAttr) -> fileAttr.isRegularFile() && filePath.toString().endsWith(".jar"))
-                    .map(j -> new MavenResolvedCoordinate("", "", "", Paths.get(j.toUri())))
-                    .collect(Collectors.toList());
-
-            //Create Classpath
-            var classpath = new ModuleClasspath(project, Optional.of(dependencies));
-
-            //Create call graph
-            var cg = WalaCallgraphConstructor.build(classpath);
-
-            //Resolve call graph into a CHA call graph
-            var CHACallgraph = WalaCallgraphConstructor.makeCHA(cg);
-
-            Function<MethodReference, UppdateraMethod> toUppdatera =
-                    m -> new UppdateraMethod(toUppdateraFunctionString(m), getClassLoader(m));
-
-
-            //Flip edges and create a reverse cg
-            var reverseCgMap = CHACallgraph
-                    .stream()
-                    .filter(call -> !getClassLoader(call.target).equals(ClassLoaderReference.Primordial))
-                    .collect(Collectors.toMap(
-                            c -> toUppdatera.apply(c.target),
-                            c -> Set.of(toUppdatera.apply(c.source)),
-                            (c1, c2) -> mergeSets(c1, c2)
-                            )
-                    );
-
-            //Set of all functions in the call graph
-            var cgNodes = CHACallgraph.stream()
-                    .flatMap(call -> Stream.of(call.target, call.source))
-                    .map(m -> toUppdatera.apply(m))
-                    .collect(Collectors.toSet());
-
-
-            //1. Evaluate if we have all functions nodes in the call graph
-            var missingFns = functions
-                    .stream()
-                    .filter(fn -> !cgNodes.contains(fn))
-                    .collect(Collectors.toList());
-
-            //Create folder for saving data
-            var ts = new SimpleDateFormat("yyyyMMddHHmmss").format(new Date());
-            Files.createDirectories(Paths.get("uppdatera", ts));
-
-            //Write the missing functions to a file
-            Files.write(Paths.get("uppdatera", ts, "missing-fns.txt"), missingFns
-                    .stream()
-                    .map(fn -> fn.name)
-                    .collect(Collectors.toList()));
-
-
-            //2. Search algorithm (per function)
-            // - only visit new keys! (never the same)
-            // - stop criteria: next key belongs to Application Loader
-            var missingPaths = functions
-                    .parallelStream()
-                    .filter(fn -> !isRechable(fn, reverseCgMap))
-                    .collect(Collectors.toList());
-
-            Files.write(Paths.get("uppdatera", ts, "missing-paths.txt"), missingPaths
-                    .stream()
-                    .map(fn -> fn.name)
-                    .collect(Collectors.toList()));
-
-            String header = "total\tmissing_nodes\tmissing_paths";
-            String results = functions.size() + "\t" + missingFns.size() + "\t" + missingPaths.size();
-            Files.write(Paths.get("uppdatera", ts, "stats.txt"), List.of(header, results));
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-    }
-
-    static class UppdateraMethod {
-
-        public final String name;
-        public final ClassLoaderReference classLoader;
-
-        public UppdateraMethod(String name, ClassLoaderReference classLoader) {
-            this.name = name;
-            this.classLoader = classLoader;
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(name, classLoader);
-        }
-
-        @Override
-        public boolean equals(Object obj) {
-            if (obj == this)
-                return true;
-
-            if (obj == null || obj.getClass() != getClass())
-                return false;
-
-            UppdateraMethod m = (UppdateraMethod) obj;
-            return Objects.equals(name, m.name) && Objects.equals(classLoader, m.classLoader);
-        }
-
-        @Override
-        public String toString() {
-            return name + ":" + classLoader.getName().toString();
-        }
-    }
 }
--- src/main/java/com/github/jhejderup/generator/WalaCallgraphConstructor.java
+++ src/main/java/com/github/jhejderup/generator/WalaCallgraphConstructor.java
@@ -10,12 +10,13 @@ import com.ibm.wala.ipa.callgraph.impl.DefaultEntrypoint;
 import com.ibm.wala.ipa.callgraph.impl.Util;
 import com.ibm.wala.ipa.cha.ClassHierarchy;
 import com.ibm.wala.ipa.cha.ClassHierarchyFactory;
-import com.ibm.wala.types.ClassLoaderReference;
 import com.ibm.wala.util.config.AnalysisScopeReader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -25,118 +26,133 @@ import static java.util.stream.Collectors.joining;
 
 public final class WalaCallgraphConstructor {
 
+  private static Logger logger = LoggerFactory
+      .getLogger(WalaCallgraphConstructor.class);
 
-    private static Logger logger = LoggerFactory.getLogger(WalaCallgraphConstructor.class);
+  public static CallGraph build(ModuleClasspath analysisClasspath,
+      String filename) {
 
-    public static CallGraph build(ModuleClasspath analysisClasspath) {
+    try {
+      var classpath = analysisClasspath.getCompleteClasspath().stream()
+          .map(c -> c.jarPath.toString()).collect(joining(":"));
 
-        try {
-            var classpath = analysisClasspath
-                    .getCompleteClasspath()
-                    .stream()
-                    .map(c -> c.jarPath.toString()).collect(joining(":"));
+      logger.info("Building call graph with classpath: {}", classpath);
+      //1. Fetch exclusion file
+      var classLoader = WalaCallgraphConstructor.class.getClassLoader();
+      var exclusionFile = new File(
+          classLoader.getResource("Java60RegressionExclusions.txt").getFile());
 
-            logger.info("Building call graph with classpath: {}", classpath);
-            //1. Fetch exclusion file
-            var classLoader = WalaCallgraphConstructor.class.getClassLoader();
-            var exclusionFile = new File(classLoader.getResource("Java60RegressionExclusions.txt").getFile());
+      var appJAR = analysisClasspath.project.jarPath.toString();
 
-            var appJAR = analysisClasspath.project.jarPath.toString();
+      //2. Set the analysis scope
+      var scope = AnalysisScopeReader
+          .makeJavaBinaryAnalysisScope(appJAR, exclusionFile);
 
-            //2. Set the analysis scope
-            var scope = AnalysisScopeReader.makeJavaBinaryAnalysisScope(appJAR, exclusionFile);
+      if (analysisClasspath.dependencies.isPresent()) {
 
-            if (analysisClasspath.dependencies.isPresent()) {
+        var depzJARpath = analysisClasspath.dependencies.get().stream()
+            .map(c -> c.jarPath.toString()).collect(joining(":"));
+        AnalysisScopeReader.addClassPathToScope(depzJARpath, scope,
+            scope.getLoader(AnalysisScope.EXTENSION));
+      }
 
-                var depzJARpath = analysisClasspath.dependencies.get().stream()
-                        .map(c -> c.jarPath.toString()).collect(joining(":"));
-                AnalysisScopeReader.addClassPathToScope(depzJARpath, scope, scope.getLoader(AnalysisScope.EXTENSION));
-            }
+      //3. Class Hierarchy for name resolution -> missing superclasses are replaced by the ClassHierarchy root,
+      //   i.e. java.lang.Object
+      var cha = ClassHierarchyFactory.makeWithRoot(scope);
 
-            //3. Class Hierarchy for name resolution -> missing superclasses are replaced by the ClassHierarchy root,
-            //   i.e. java.lang.Object
-            var cha = ClassHierarchyFactory.makeWithRoot(scope);
+      //4. Both Private/Public functions are entry-points
+      var entryPoints = makeEntryPoints(scope, cha);
 
-            //4. Both Private/Public functions are entry-points
-            var entryPoints = makeEntryPoints(cha);
+      //5. Encapsulates various analysis options
+      var options = new AnalysisOptions(scope, entryPoints);
+      var cache = new AnalysisCacheImpl();
 
-            //5. Encapsulates various analysis options
-            var options = new AnalysisOptions(scope, entryPoints);
-            var cache = new AnalysisCacheImpl();
+      //6 Build the call graph
+      var builder = Util.makeRTABuilder(options, cache, cha, scope);
+      var cg = builder.makeCallGraph(options, null);
 
-            //6 Build the call graph
-            var builder = Util.makeRTABuilder(options, cache, cha, scope);
-            return builder.makeCallGraph(options, null);
+      var depclasses = itrToStream(cha.iterator()).filter(c -> !c.isInterface())
+          .filter(c -> isExtension(scope, c)).map(c -> c.getName())
+          .map(t -> t.toString().substring(1));
 
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        }
+      Files.write(Paths.get(filename, "wala-dep-cha.txt"),
+          (Iterable<String>) depclasses::iterator);
+      return cg;
+
+    } catch (Exception e) {
+      e.printStackTrace();
+      return null;
     }
+  }
 
-    private static <T> Stream<T> itrToStream(Iterator<T> itr) {
-        Iterable<T> iterable = () -> itr;
-        return StreamSupport.stream(iterable.spliterator(), false);
+  private static <T> Stream<T> itrToStream(Iterator<T> itr) {
+    Iterable<T> iterable = () -> itr;
+    return StreamSupport.stream(iterable.spliterator(), false);
+  }
+
+  public static List<ResolvedCall> makeCHA(CallGraph cg) {
+    //Get all entrypoints and turn into a J8 Stream
+    var entryPointsStream = itrToStream(
+        cg.getFakeRootNode().iterateCallSites());
+
+    //Place initial nodes in a work list
+    var workList = entryPointsStream.map(CallSiteReference::getDeclaredTarget)
+        .collect(Collectors.toCollection(Stack::new));
+
+    var visited = new HashSet<>();
+    var calls = new ArrayList<ResolvedCall>();
+
+    while (!workList.empty()) {
+      var srcMref = workList.pop();
+      //Resolve ref to impl
+      var resolveMethod = cg.getClassHierarchy().resolveMethod(srcMref);
+
+      cg.getNodes(srcMref).stream().forEach(cgNode -> {
+        itrToStream(cgNode.iterateCallSites())
+            .flatMap(cs -> cg.getPossibleTargets(cgNode, cs).stream())
+            .map(n -> n.getMethod().getReference()).forEach(csMref -> {
+          if (!visited.contains(csMref)) {
+            workList.add(csMref);
+            visited.add(csMref);
+          }
+          if (resolveMethod != null) {
+            calls.add(new ResolvedCall(resolveMethod.getReference(), csMref));
+          }
+        });
+      });
     }
+    return calls;
+  }
 
-    public static List<ResolvedCall> makeCHA(CallGraph cg) {
-        //Get all entrypoints and turn into a J8 Stream
-        var entryPointsStream = itrToStream(cg.getFakeRootNode().iterateCallSites());
+  private static ArrayList<Entrypoint> makeEntryPoints(AnalysisScope scope,
+      ClassHierarchy cha) {
+    Iterable<IClass> classes = () -> cha.iterator();
+    var entryPoints = StreamSupport.stream(classes.spliterator(), false)
+        .filter(clazz -> skipInterface(scope, clazz))
+        .flatMap(clazz -> clazz.getDeclaredMethods().parallelStream())
+        .filter(m -> skipAbstractMethod(scope, m))
+        .map(m -> new DefaultEntrypoint(m, cha)).collect(Collectors.toList());
+    return new ArrayList<>(entryPoints);
+  }
 
-        //Place initial nodes in a work list
-        var workList = entryPointsStream
-                .map(CallSiteReference::getDeclaredTarget)
-                .collect(Collectors.toCollection(Stack::new));
+  private static boolean skipInterface(AnalysisScope scope, IClass klass) {
+    return !klass.isInterface() && isApplication(scope, klass);
+  }
 
-        var visited = new HashSet<>();
-        var calls = new ArrayList<ResolvedCall>();
+  private static boolean skipAbstractMethod(AnalysisScope scope,
+      IMethod method) {
+    return !method.isAbstract() && isApplication(scope,
+        method.getDeclaringClass());
+  }
 
-        while (!workList.empty()) {
-            var srcMref = workList.pop();
-            //Resolve ref to impl
-            var resolveMethod = cg.getClassHierarchy().resolveMethod(srcMref);
+  public static Boolean isApplication(AnalysisScope scope, IClass klass) {
+    return scope.getApplicationLoader()
+        .equals(klass.getClassLoader().getReference());
+  }
 
-            cg.getNodes(srcMref)
-                    .stream()
-                    .forEach(cgNode -> {
-                        itrToStream(cgNode.iterateCallSites())
-                                .flatMap(cs -> cg.getPossibleTargets(cgNode, cs).stream())
-                                .map(n -> n.getMethod().getReference())
-                                .forEach(csMref -> {
-                                    if (!visited.contains(csMref)) {
-                                        workList.add(csMref);
-                                        visited.add(csMref);
-                                    }
-                                    if (resolveMethod != null) {
-                                        calls.add(new ResolvedCall(resolveMethod.getReference(), csMref));
-                                    }
-                                });
-                    });
-        }
-        return calls;
-    }
-
-    private static ArrayList<Entrypoint> makeEntryPoints(ClassHierarchy cha) {
-        Iterable<IClass> classes = () -> cha.iterator();
-        var entryPoints = StreamSupport.stream(classes.spliterator(), false)
-                .filter(WalaCallgraphConstructor::skipInterface)
-                .flatMap(clazz -> clazz.getAllMethods().parallelStream())
-                .filter(WalaCallgraphConstructor::skipAbstractMethod)
-                .map(m -> new DefaultEntrypoint(m, cha))
-                .collect(Collectors.toList());
-        return new ArrayList<>(entryPoints);
-    }
-
-    private static boolean skipInterface(IClass klass) {
-        return isApplication(klass) && !klass.isInterface();
-    }
-
-    private static boolean skipAbstractMethod(IMethod method) {
-        return isApplication(method.getDeclaringClass()) && !method.isAbstract();
-    }
-
-    public static Boolean isApplication(IClass klass) {
-        return klass.getClassLoader().getReference().equals(ClassLoaderReference.Application);
-    }
+  public static Boolean isExtension(AnalysisScope scope, IClass klass) {
+    return scope.getExtensionLoader()
+        .equals(klass.getClassLoader().getReference());
+  }
 
 }
